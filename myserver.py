1fdf51d04477f1743897fe27fa6b2cdfba68b059
Watcharintornki <Watcharintorn.ki@gmail.com>
2025-06-13 09:46:31 +0700

    Update myserver.py

--- myserver.py
+++ myserver.py
@@ -1,13 +1,12 @@
 from flask import Flask
 import os
-import main  # 👈 import เพื่อให้บอททำงาน
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Server is running and bot is alive!"
+    return "Server is running!"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
