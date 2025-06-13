5997566306f5ce5e9286738705b8751f73a81140
Watcharintornki <Watcharintorn.ki@gmail.com>
2025-06-13 10:07:08 +0700

    Update myserver.py

--- myserver.py
+++ myserver.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
@@ -8,5 +7,5 @@ def home():
     return "Server is running!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = 8080  # กำหนดพอร์ตตรงนี้เลย เช่น 5000
     app.run(host='0.0.0.0', port=port)
